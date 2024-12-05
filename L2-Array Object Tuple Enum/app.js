1cd57e593e88e05012323660e75ecc256f3ed6bb
Swarnavo2003 <swarnavo2020@gmail.com>
2024-12-05 23:13:58 +0530

    object array finished

---
+++ L2-Array Object Tuple Enum/app.js
@@ -0,0 +1,15 @@
+//! Object
+// const person: { firstName: string; age: number; xyz: { address: string } } = {
+//   firstName: "Swarnavo",
+//   age: 21,
+//   xyz: {
+//     address: "Kolkats",
+//   },
+// };
+//! Array
+var person = {
+    firstName: "Swarnavo",
+    age: 21,
+    skills: ["Reactjs", "Nodejs"],
+};
+console.log(person);
---
+++ L2-Array Object Tuple Enum/app.ts
@@ -0,0 +1,17 @@
+//! Object
+// const person: { firstName: string; age: number; xyz: { address: string } } = {
+//   firstName: "Swarnavo",
+//   age: 21,
+//   xyz: {
+//     address: "Kolkats",
+//   },
+// };
+
+//! Array
+const person: { firstName: string; age: number; skills: string[] } = {
+  firstName: "Swarnavo",
+  age: 21,
+  skills: ["Reactjs", "Nodejs"],
+};
+
+console.log(person);
